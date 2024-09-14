aa63e3da61e039dcccbbe0d60684ee13f5c1691b
Sagar9178 <sagarkalia6371@gmail.com>
2024-09-14 13:42:49 +0530

    Next Permutation

--- 304/RangeSumQuery2DImmutable.java
+++ 304/RangeSumQuery2DImmutable.java
@@ -34,5 +34,34 @@
 // At most 104 calls will be made to sumRegion.
 
 class RangeSumQuery2DImmutable{
-
+    int[][] find=new int[200][200];
+    public RangeSumQuery2DImmutable(int[][] matrix) {
+        int n=matrix.length;
+        int m=matrix[0].length;
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(i==0&&j==0){
+                    find[i][j]=matrix[i][j];
+                }
+                else if(i==0){
+                    find[i][j]=find[i][j-1]+matrix[i][j];
+                }else if(j==0){
+                    find[i][j]=find[i-1][j]+matrix[i][j];
+                }else{
+                    find[i][j]=find[i-1][j]+find[i][j-1]-find[i-1][j-1]+matrix[i][j];
+                }
+            }
+        }
+    }
+    
+    public int sumRegion(int r1, int c1, int r2, int c2) {
+       int ans=find[r2][c2];
+       if(r1>0){ans-=find[r1-1][c2];}
+       if(c1>0){ans-=find[r2][c1-1];}
+       if(r1>0&&c1>0){ans+=find[r1-1][c1-1];}
+       return ans;
+        
+        
+        
+    }
 }
